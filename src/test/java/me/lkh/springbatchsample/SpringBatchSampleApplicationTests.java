a98c0b1d6b8c368b67ad9c61d6b7c188ea9c5ffd
lkhzz <leta6603@gmail.com>
2023-10-11 23:00:05 +0900

    tasklet 생성

--- src/test/java/me/lkh/springbatchsample/SpringBatchSampleApplicationTests.java
+++ src/test/java/me/lkh/springbatchsample/SpringBatchSampleApplicationTests.java
@@ -14,6 +14,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @SpringBatchTest	// JobLauncherTestUtils, JobRepositoryTestUtils를 빈으로 등록해줍니다.
 @SpringBootTest
 @ExtendWith(OutputCaptureExtension.class)	// spring boot가 제공하는 기능, 콘솔에 찍히는 output을 캡처할 수 있습니다.
@@ -38,6 +41,24 @@ class SpringBatchSampleApplicationTests {
 		this.jobRepositoryTestUtils.removeJobExecutions();	// 각각의 테스트별로 JobExecution을 정리합니다.
 															// 이를 통해 여러번 테스트를 수행해도 Job Instance가 이미 존재해서 테스트가 실패하는 일을 막을 수 있습니다.
 	}
+
+	@Test
+	void testJobExecution() throws Exception {
+		// given
+		JobParameters jobParameters = new JobParametersBuilder()
+				.addString("input.file", "src/main/resources/billing-2023-01.csv")
+				.toJobParameters();
+
+		// when
+		JobExecution jobExecution = this.jobLauncherTestUtils.launchJob(jobParameters);
+
+		// then
+		Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+		Assertions.assertTrue(Files.exists(Paths.get("staging", "billing-2023-01.csv")));
+	}
+
+	/*
+	// test v3
 	@Test
 	void testJobExecution(CapturedOutput output) throws Exception {
 		// given
@@ -53,6 +74,7 @@ class SpringBatchSampleApplicationTests {
 		Assertions.assertTrue(output.getOut().contains("processing billing information from file " + parameterFileName));
 		Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
 	}
+	 */
 
 	/*
 	// test version 2
